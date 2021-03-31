44114a85fcb7fa3810a296a0bfb1a9b2708dd777
Ashdown <rory.devane@gmail.com>
2021-03-31 23:56:38 +0100

    Added some basic styles

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    root: {
+        padding: 16,
+        backgroundColor: '#ccc'
+    },
+    pageTitle: {
+        fontFamily: ["'Roboto'", 'sans-serif'],
+        marginTop: 16,
+        fontSize: 32,
+    },
+    paragraph: {
+        fontFamily: ["'Roboto'", 'sans-serif'],
+        marginTop: 16,
+        fontSize: 16
+    }
+})
 
 function TestPage(): React.ReactElement {
 
+    const styles = useStyles()
+
     return (
-        <div>
-            <h1>Test page</h1>
-            <p>This is the test page</p>
+        <div className={styles.root}>
+            <h1 className={styles.pageTitle}>Test page</h1>
+            <p className={styles.paragraph}>This is the test page</p>
         </div>
     )
 }
